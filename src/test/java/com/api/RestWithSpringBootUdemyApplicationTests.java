292f00ca1bc0093c6cb3bb7ef24de33163dd65dd
admin <luciano.desenvolvedor@gmail.com>
2020-08-27 12:53:14 -0300

    Dados iniciais do proprojeto

---
+++ src/test/java/com/api/RestWithSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
